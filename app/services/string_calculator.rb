037e32968729ff630d7b4c2a175f8f952ffe3a4f
Bharat Goswami <goswamirudra825@gmail.com>
2025-11-09 12:19:57 +0530

    add some another logic

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -4,19 +4,19 @@ class StringCalculator
 
     negatives = []
     sum = 0
+    delimiters = [',']
 
     if numbers.start_with?("//")
-      delimiter = numbers[2]
-      numbers = numbers[4..-1]
-    else
-      delimiter = ","
+      delimiter_section, numbers = numbers.split("\n", 2)
+      delimiters = extract_delimiters(delimiter_section)
     end
 
-    numbers.split(/[\n#{delimiter}]/).each do |num|
+    numbers.split(/[\n#{delimiters.join}]/).each do |num|
       number = num.to_i
+
       if number < 0
         negatives << number
-      else
+      elsif number <= 1000
         sum += number
       end
     end
@@ -26,5 +26,14 @@ class StringCalculator
     end
 
     sum
+  end
+
+  private
+  def self.extract_delimiters(delimiter_section)
+    if delimiter_section.start_with?("//[")
+      delimiter_section[3..-2].split('][')
+    else
+      [delimiter_section[2]]
+    end
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 
 RSpec.describe StringCalculator, type: :service do
@@ -23,8 +22,28 @@ RSpec.describe StringCalculator, type: :service do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
 
+    it 'supports multiple delimiters with custom length' do
+      expect(StringCalculator.add("//[*][%]\n1*2%3")).to eq(6)
+    end
+
+    it 'supports multi-character custom delimiters' do
+      expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it 'ignores numbers greater than 1000' do
+      expect(StringCalculator.add("2,1001")).to eq(2)
+    end
+
     it 'raises an error for negative numbers' do
       expect { StringCalculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2, -3")
+    end
+
+    it 'raises an error for a single negative number' do
+      expect { StringCalculator.add("-1") }.to raise_error("negative numbers not allowed: -1")
+    end
+
+    it 'ignores numbers greater than 1000 and sums the valid numbers' do
+      expect(StringCalculator.add("2,1001,1000")).to eq(1002)
     end
   end
 end
